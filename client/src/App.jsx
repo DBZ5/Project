4b6265687add021782f9b82a33af862e4bf40110
Ademchibani <ademfootbal@gmail.com>
2025-01-29 16:12:56 +0100

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,12 @@
 import Darkmode from "darkmode-js";
+<<<<<<< HEAD
+import SignUp from "./components/SignUp";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { NavLink } from "react-router-dom";
+import Cart from "./components/Cart";
+import Payment from "./components/Payment";
+=======
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -8,11 +16,7 @@ import MainPage from './components/MainPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from "./components/About";
-import Contact from "./components/Contact";
-import { NavLink } from "react-router-dom";
-import Cart from "./components/Cart";
-import Payment from "./components/Payment";
+>>>>>>> 201badb22eb9018d9567e3537f3bf1c45c1224d3
 
 function App() {
   const options = {
@@ -34,13 +38,36 @@ function App() {
   const GOOGLE_CLIENT_ID = "181292479338-qu3s0buf3v2rqn891qcg9ca3pjdadkoc.apps.googleusercontent.com"; // Replace with your Google Client ID
 
   return (
+<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
       </Routes>
     </Router>
+=======
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <MainPage />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
+>>>>>>> 201badb22eb9018d9567e3537f3bf1c45c1224d3
   );
 }
 
